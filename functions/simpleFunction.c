263171619e447411c6c9e8e003d4b114496373ea
Harshdeep Mishra <mishraharshdeep743@gmail.com>
2025-05-30 22:57:04 +0530

    simple function

---
+++ functions/simpleFunction.c
@@ -0,0 +1,10 @@
+#include <Stdio.h>
+void message();
+int main(){
+    message();
+    printf("Hello, World!\n");
+    return 0;
+}
+void message() {
+    printf("This is a simple function in C.\n");
+}
