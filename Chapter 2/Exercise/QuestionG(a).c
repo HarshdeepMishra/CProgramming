e25498116cea797c2c637cc14f9a8fa956e4be71
Harshdeep Mishra <mishraharshdeep743@gmail.com>
2025-07-02 14:11:07 +0530

    Create QuestionG(a).c

---
+++ Chapter 2/Exercise/QuestionG(a).c
@@ -0,0 +1,6 @@
+/*
+If a five digt number is input through the keyboard, write a program to calculate the sum of its digits and display the result on the screen.
+*/
+#include <stdio.h>
+int main(){
+}
