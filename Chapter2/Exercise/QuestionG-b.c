fa3487d7c65c125e435637c2808d3d1706d7bd73
Harshdeep Mishra <mishraharshdeep743@gmail.com>
2025-07-22 11:12:00 +0530

    ??

--- Chapter2/Exercise/QuestionG-b.c
+++ Chapter2/Exercise/QuestionG-b.c
@@ -5,6 +5,7 @@ r = sqrt(x^2 + y^2)
 θ = tan-1(y/x)
 */
 #include <stdio.h>
+#include <math.h>
 
 int main(){
     double x, y, r, theta;
@@ -12,5 +13,9 @@ int main(){
     printf("Enter the cartesion coordinates (x, y): ");
     scanf("%lf %lf", &x, &y);
     r = sqrt(x * x + y * y);
+    theta = atan2(y, x); // atan2 handles the quadrant correctly
+    printf("Polar coordinates:\n");
+    printf("Radius (r): %.2g\n", r);
+    printf("Angle (θ) in radians: %.2g\n", theta);
     
 }
